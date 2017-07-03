8b871b27858c8893df85dc12e023c4428a9862d1
Anton Gunov <anton@gunov.name>
2017-07-03 15:41:35 +0700

    Support for EditorConfig

--- server/api/index.js
+++ server/api/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 
 router.get('/grab/:domain', (req, res) => {
-    res.status(200).json({
-        domain: req.params.domain
-    });
+  res.status(200).json({
+    domain: req.params.domain
+  });
 });
 
 module.exports = router;
