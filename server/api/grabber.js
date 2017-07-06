bd9b5512f8c7643abe50880c9c1738badb29bafe
Anton Gunov <anton@gunov.name>
2017-07-06 16:06:52 +0700

    Added Cheerio

--- server/api/grabber.js
+++ server/api/grabber.js
@@ -1,12 +1,36 @@
 const request = require('request');
+const cheerio = require('cheerio');
+const resolve = require('url').resolve;
+
+const selectors = [
+  'link[rel="icon"]',
+];
 
 module.exports = (domain, done) => {
   const uri = `http://${domain}/`;
+  const icons = [];
+
   request(uri, (err, res, body) => {
     /* eslint no-unused-vars: off */
     if (err) return done(err);
+
+    const $ = cheerio.load(body);
+
+    selectors.forEach((selector) => {
+      $(selector, 'head').each((i, elem) => {
+        const { href, sizes, type } = elem.attribs;
+        const icon = {
+          sizes,
+          src: href[0] === '/' ? resolve(uri, href) : href,
+          type,
+        };
+        icons.push(icon);
+      });
+    });
+
     return done(null, {
       domain,
+      icons,
     });
   });
 };
