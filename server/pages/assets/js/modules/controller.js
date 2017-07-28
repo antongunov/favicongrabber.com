45c6784d2795b0ea6ca86b70fbdfbedb781af689
Anton Gunov <anton@gunov.name>
2017-07-28 18:33:29 +0700

    Updated Controller

--- server/pages/assets/js/modules/controller.js
+++ server/pages/assets/js/modules/controller.js
@@ -1,6 +1,19 @@
+const events = [];
+
 function Controller() {
   if (!(this instanceof Controller)) return new Controller();
 }
+
+Controller.prototype.$on = function (event, fn) {
+  events[event] = events[event] || [];
+  events[event].push(fn);
+};
+
+Controller.prototype.$emit = function (event, ...args) {
+  if (events[event]) {
+    events[event].forEach(fn => fn(...args));
+  }
+};
 
 Controller.prototype.$proxy = function (fn) {
   return fn.bind(this);
--- server/pages/assets/js/modules/domain-form.js
+++ server/pages/assets/js/modules/domain-form.js
@@ -9,17 +9,24 @@ function domainForm (Controller) {
     const domainInput = ctrl.domainForm.querySelector('input[type="text"]');
     const value = domainInput.value;
 
-    if (!value || !(typeof value === 'string' || value instanceof String)) {
-       return null;
+    if (!value) return ctrl.$emit('error', 'No domain provided.');
+
+    let domain = null;
+    let url = null;
+
+    // TODO: how to improve parsing domain?
+    if (/[:@/]/.test(value)) {
+      try {
+        url = new URL(value);
+        if (!url.hostname) return ctrl.$emit('error', 'This domain is invalid.');
+        domain = url.hostname;
+      } catch (err) {
+        return ctrl.$emit('error', 'This domain is invalid.');
+      }
+    } else {
+      domain = value;
     }
 
-    const url = new URL(value);
-
-    if (!url.hostname) {
-      return null;
-    }
-
-    const domain = url.hostname;
     domainInput.value = domain;
     return domain;
   };
@@ -35,5 +42,7 @@ function domainForm (Controller) {
   ctrl.$load(function () {
     ctrl.domainForm = document.querySelector('#domain-form');
     ctrl.domainForm.onsubmit = ctrl.$proxy(ctrl.tryItGrab);
+
+    ctrl.$on('error', msg => console.error(msg));
   });
 }
