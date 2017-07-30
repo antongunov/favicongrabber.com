d59b014350fa440b9db272ea1b2fbf258b030157
Anton Gunov <anton@gunov.name>
2017-07-30 11:55:21 +0700

    Added request module

--- server/pages/assets/js/modules/form-domain.js
+++ server/pages/assets/js/modules/form-domain.js
@@ -1,3 +1,4 @@
+const request = require('./request');
 require('url-polyfill');
 
 module.exports = formDomain;
@@ -5,36 +6,63 @@ module.exports = formDomain;
 function formDomain(Controller) {
   const ctrl = new Controller();
 
-  ctrl.extractDomain = function () {
-    const inputDomain = ctrl.formDomain.querySelector('input[type="text"]');
-    const value = inputDomain.value;
-
-    if (!value) return ctrl.$emit('error', 'No domain provided.');
-
+  ctrl.extractDomain = function (value) {
     let domain = null;
     let url = null;
 
     if (/[:@/]/.test(value)) {
       try {
         url = new URL(value);
-        if (!url.hostname) return ctrl.$emit('error', 'This domain is invalid.');
         domain = url.hostname;
       } catch (err) {
-        return ctrl.$emit('error', 'This domain is invalid.');
+
       }
     } else {
       domain = value;
     }
 
-    inputDomain.value = domain;
     return domain;
   };
 
   ctrl.tryItGrab = function () {
+    const inputDomain = ctrl.formDomain.querySelector('input[type="text"]');
+    const value = inputDomain.value;
+
+    if (!value) {
+      ctrl.$emit('error', 'No domain provided.');
+      return false;
+    }
+
+    const domain = ctrl.extractDomain(value);
+
+    if (!domain) {
+      ctrl.$emit('error', 'This domain is invalid.');
+      return false;
+    }
+
+    inputDomain.value = domain;
+
     ctrl.$emit('begin-grabbing');
 
-    const domain = ctrl.extractDomain();
-    console.log(domain);
+    request(`http://favicongrabber.com/api/grab/${domain}`, function (err, status, res) {
+      if (err) {
+        console.error(err);
+        ctrl.$emit('error', 'General UI error.');
+        return false;
+      }
+
+      switch (status) {
+        case 200:
+          ctrl.$emit('end-grabbing', res);
+          break;
+        case 500:
+          ctrl.$emit('error', res.error);
+          break;
+        default:
+          console.error(`An unrecognized HTTP status ${status} was received.`);
+          ctrl.$emit('error', 'General UI error.');
+      }
+    });
 
     // prevent form submission
     return false;
