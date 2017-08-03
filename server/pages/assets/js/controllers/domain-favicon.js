2c8c614527a71a323c401ffbf958004915728ccb
Anton Gunov <anton@gunov.name>
2017-08-03 19:04:51 +0700

    Added download button for image

--- server/pages/assets/js/controllers/domain-favicon.js
+++ server/pages/assets/js/controllers/domain-favicon.js
@@ -13,8 +13,8 @@ module.exports = (Controller) => {
     const img = new Image();
 
     img.onload = function () {
-      ctrl.domainFaviconCmp.image.innerHTML = '';
-      ctrl.domainFaviconCmp.image.appendChild(this);
+      ctrl.domainFaviconCmp.imageContainer.image.innerHTML = '';
+      ctrl.domainFaviconCmp.imageContainer.image.appendChild(this);
 
       ctrl.$emit('end-downloading', icon);
     };
@@ -36,7 +36,7 @@ module.exports = (Controller) => {
     ctrl.domainFaviconCmp = {
       errorMessage: cmp.querySelector(`.${nameCtrl}__error-message`),
       spinner: cmp.querySelector(`.${nameCtrl}__spinner`),
-      image: cmp.querySelector(`.${nameCtrl}__image`),
+      imageContainer: cmp.querySelector(`.${nameCtrl}__image-container`),
 
       active(name) {
         Object.keys(this).forEach((key) => {
@@ -44,15 +44,21 @@ module.exports = (Controller) => {
             this[key].style['display'] = 'none';
           }
         });
-        this[name].style['display'] = 'block';
+        this[name].style['display'] = 'inline-block';
       }
     };
+
+    ctrl.domainFaviconCmp.imageContainer.image = ctrl.domainFaviconCmp.imageContainer.querySelector(`.${nameCtrl}__image`);
+    ctrl.domainFaviconCmp.imageContainer.downloadButton = ctrl.domainFaviconCmp.imageContainer.querySelector(`.${nameCtrl}__download-button .download-button`);
 
     ctrl.$on('domain-form:error', ctrl.$proxy(ctrl.onError));
     ctrl.$on('domain-form:begin-grabbing', () => ctrl.domainFaviconCmp.active('spinner'));
     ctrl.$on('domain-form:end-grabbing', ctrl.$proxy(ctrl.onEndGrabbing));
 
     ctrl.$on(`${nameCtrl}:error`, ctrl.$proxy(ctrl.onError));
-    ctrl.$on(`${nameCtrl}:end-downloading`, () => ctrl.domainFaviconCmp.active('image'));
+    ctrl.$on(`${nameCtrl}:end-downloading`, (icon) => {
+      ctrl.domainFaviconCmp.imageContainer.downloadButton.href = `/download/${icon.src}`;
+      ctrl.domainFaviconCmp.active('imageContainer');
+    });
   });
 };
