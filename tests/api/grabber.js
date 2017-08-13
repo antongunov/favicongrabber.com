1ab56a52c96cdf6c89e9f35a3f3d88398a7e6de5
Anton Gunov <anton@gunov.name>
2017-08-13 17:43:47 +0700

    Support for Cyrillic domain name

--- tests/api/grabber.js
+++ tests/api/grabber.js
@@ -1,16 +1,17 @@
 const supertest = require('supertest');
-const { readdirSync } = require('fs');
+const readdirSync = require('fs').readdirSync;
 
 const NODE_PORT = parseInt(process.env.NODE_PORT, 10);
 
 describe('API Grabber', () => {
   const request = supertest(`http://localhost:${NODE_PORT}`);
+
   readdirSync('./tests/api/data')
     .forEach((filename) => {
       const data = require(`./data/${filename}`);
       it(`GET /api/grab/${data.domain}`, (done) => {
         request
-          .get(`/api/grab/${data.domain}`)
+          .get(encodeURI(`/api/grab/${data.domain}`))
           .expect(200, data)
           .end(done);
       });
