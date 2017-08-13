12215debab026a701320991de9c9b8fe0649ffb5
Anton Gunov <anton@gunov.name>
2017-08-13 18:04:24 +0700

    Implementation of grabbing /favicon.ico

--- server/api/grabber/favicon.js
+++ server/api/grabber/favicon.js
@@ -1,4 +1,20 @@
+const baseRequest = require('./base-request');
+const URL = require('url').URL;
+
 module.exports = ($, done) => {
-  // TODO: not implemented
-  return done(null, []);
+  const url = new URL('/favicon.ico', $.baseUrl).href;
+  const reqOptions = {
+    method: 'HEAD',
+  };
+
+  baseRequest(url, reqOptions, (err, res) => {
+    // ignore errors
+    if (err) return done(null, []);
+    if (res.statusCode !== 200) return done(null, []);
+
+    return done(null, [{
+      src: url,
+      type: 'image/x-icon',
+    }]);
+  });
 };
