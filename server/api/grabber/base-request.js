55de3ab0633ec7af1aca982c0e8041c78d86a165
Anton Gunov <anton@gunov.name>
2017-08-15 14:38:51 +0700

    Updated User-Agent header

--- server/api/grabber/base-request.js
+++ server/api/grabber/base-request.js
@@ -6,7 +6,7 @@ module.exports = request.defaults({
   headers: {
     'Accept': '*/*',
     // prevent to redirect to the mobile version of a website
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64; rv:54.0) Gecko/20100101 Firefox/54.0',
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:54.0) Gecko/20100101 Firefox/54.0',
   },
   timeout: 5000,
 });
