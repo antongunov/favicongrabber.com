4de62a8544e62ff0f1c107485308b5da593180a9
Anton Gunov <anton@gunov.name>
2018-03-22 23:37:39 +0300

    Rename GA util and send method

--- assets/js/utils/ga.js
+++ assets/js/utils/ga.js
@@ -2,16 +2,22 @@ const warning = require('./warning');
 
 /**
  * Send an event to Google Analytics.
+ *
  * @param {string} category
  * @param {string} action
  * @param {string} [label]
  * @param {number} [value] Value must be non-negative.
+ * @returns {void}
  */
-module.exports = (category, action, label, value) => {
+function event(category, action, label, value) {
   if (!window.ga) {
     warning('Google Analytics module not found.');
     return;
   }
 
   window.ga('send', 'event', category, action, label, value);
+}
+
+module.exports = {
+  event,
 };
