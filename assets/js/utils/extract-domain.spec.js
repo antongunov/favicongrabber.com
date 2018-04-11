62fd29a4ebd4c0bcf40e00f8cd93208fdd13a469
Anton Gunov <anton@gunov.name>
2018-04-11 23:41:27 +0300

    New tests: url has a port number, an user with(out) a password

--- assets/js/utils/extract-domain.spec.js
+++ assets/js/utils/extract-domain.spec.js
@@ -1,5 +1,19 @@
 const extractDomain = require('./extract-domain');
 
+const EXAMPLE_COM = 'example.com';
+
 it('url parameter is required', () => {
   expect(() => extractDomain()).toThrow(/required/);
+});
+
+it('url has a port number', () => {
+  expect(extractDomain(`${EXAMPLE_COM}:8080`)).toEqual(EXAMPLE_COM);
+});
+
+it('url has an user', () => {
+  expect(extractDomain(`user@${EXAMPLE_COM}`)).toEqual(EXAMPLE_COM);
+});
+
+it('url has an user with a password', () => {
+  expect(extractDomain(`user:password@${EXAMPLE_COM}`)).toEqual(EXAMPLE_COM);
 });
