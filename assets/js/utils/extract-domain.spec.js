28d8fd84909186e4e8f7887e8b0cfd86ff6b67a3
Anton Gunov <anton@gunov.name>
2018-04-13 00:00:33 +0300

    New tests: url has a path and a protocol

--- assets/js/utils/extract-domain.spec.js
+++ assets/js/utils/extract-domain.spec.js
@@ -7,13 +7,31 @@ it('url parameter is required', () => {
 });
 
 it('url has a port number', () => {
-  expect(extractDomain(`${EXAMPLE_COM}:8080`)).toEqual(EXAMPLE_COM);
+  expect(extractDomain(`${EXAMPLE_COM}:8080`))
+    .toEqual(EXAMPLE_COM);
 });
 
 it('url has an user', () => {
-  expect(extractDomain(`user@${EXAMPLE_COM}`)).toEqual(EXAMPLE_COM);
+  expect(extractDomain(`user@${EXAMPLE_COM}`))
+    .toEqual(EXAMPLE_COM);
 });
 
 it('url has an user with a password', () => {
-  expect(extractDomain(`user:password@${EXAMPLE_COM}`)).toEqual(EXAMPLE_COM);
+  expect(extractDomain(`user:password@${EXAMPLE_COM}`))
+    .toEqual(EXAMPLE_COM);
+});
+
+it('url has a path', () => {
+  expect(extractDomain(`${EXAMPLE_COM}/about`))
+    .toEqual(EXAMPLE_COM);
+});
+
+it('url has a protocol', () => {
+  expect(extractDomain(`https://${EXAMPLE_COM}`))
+    .toEqual(EXAMPLE_COM);
+});
+
+it('url has a protocol and a path', () => {
+  expect(extractDomain(`https://${EXAMPLE_COM}/about`))
+    .toEqual(EXAMPLE_COM);
 });
