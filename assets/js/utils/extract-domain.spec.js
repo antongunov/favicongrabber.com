6145bc3916edd1370b3a1207913baafebc9468a1
Anton Gunov <anton@gunov.name>
2018-04-16 23:40:45 +0300

    Replace it (alias) to test method

--- assets/js/utils/extract-domain.spec.js
+++ assets/js/utils/extract-domain.spec.js
@@ -2,46 +2,46 @@ const extractDomain = require('./extract-domain');
 
 const EXAMPLE_COM = 'example.com';
 
-it('url parameter is required', () => {
+test('url parameter is required', () => {
   expect(() => extractDomain()).toThrow(/required/);
 });
 
-it('url is a domain', () => {
+test('url is a domain', () => {
   expect(extractDomain(EXAMPLE_COM))
     .toEqual(EXAMPLE_COM);
 });
 
-it('url has a port number', () => {
+test('url has a port number', () => {
   expect(extractDomain(`${EXAMPLE_COM}:8080`))
     .toEqual(EXAMPLE_COM);
 });
 
-it('url has an user', () => {
+test('url has an user', () => {
   expect(extractDomain(`user@${EXAMPLE_COM}`))
     .toEqual(EXAMPLE_COM);
 });
 
-it('url has an user with a password', () => {
+test('url has an user with a password', () => {
   expect(extractDomain(`user:password@${EXAMPLE_COM}`))
     .toEqual(EXAMPLE_COM);
 });
 
-it('url has a path', () => {
+test('url has a path', () => {
   expect(extractDomain(`${EXAMPLE_COM}/about`))
     .toEqual(EXAMPLE_COM);
 });
 
-it('url has a protocol', () => {
+test('url has a protocol', () => {
   expect(extractDomain(`https://${EXAMPLE_COM}`))
     .toEqual(EXAMPLE_COM);
 });
 
-it('url has a protocol and a path', () => {
+test('url has a protocol and a path', () => {
   expect(extractDomain(`https://${EXAMPLE_COM}/about`))
     .toEqual(EXAMPLE_COM);
 });
 
-it('url has a protocol and path with multiple slashes', () => {
+test('url has a protocol and path with multiple slashes', () => {
   expect(extractDomain(`https://${EXAMPLE_COM}///about`))
     .toEqual(EXAMPLE_COM);
 });
