d1842ea278438430b5df811012421e0431c8f856
Anton Gunov <anton@gunov.name>
2018-04-17 23:14:34 +0300

    Improve expecting example.com

--- assets/js/utils/extract-domain.spec.js
+++ assets/js/utils/extract-domain.spec.js
@@ -2,46 +2,42 @@ const extractDomain = require('./extract-domain');
 
 const EXAMPLE_COM = 'example.com';
 
+function expectExampleCom(url) {
+  expect(extractDomain(url)).toEqual(EXAMPLE_COM);
+}
+
 test('url parameter is required', () => {
   expect(() => extractDomain()).toThrow(/required/);
 });
 
 test('url is a domain', () => {
-  expect(extractDomain(EXAMPLE_COM))
-    .toEqual(EXAMPLE_COM);
+  expectExampleCom(EXAMPLE_COM);
 });
 
 test('url has a port number', () => {
-  expect(extractDomain(`${EXAMPLE_COM}:8080`))
-    .toEqual(EXAMPLE_COM);
+  expectExampleCom(`${EXAMPLE_COM}:8080`);
 });
 
 test('url has an user', () => {
-  expect(extractDomain(`user@${EXAMPLE_COM}`))
-    .toEqual(EXAMPLE_COM);
+  expectExampleCom(`user@${EXAMPLE_COM}`);
 });
 
 test('url has an user with a password', () => {
-  expect(extractDomain(`user:password@${EXAMPLE_COM}`))
-    .toEqual(EXAMPLE_COM);
+  expectExampleCom(`user:password@${EXAMPLE_COM}`);
 });
 
 test('url has a path', () => {
-  expect(extractDomain(`${EXAMPLE_COM}/about`))
-    .toEqual(EXAMPLE_COM);
+  expectExampleCom(`${EXAMPLE_COM}/about`);
 });
 
 test('url has a protocol', () => {
-  expect(extractDomain(`https://${EXAMPLE_COM}`))
-    .toEqual(EXAMPLE_COM);
+  expectExampleCom(`https://${EXAMPLE_COM}`);
 });
 
 test('url has a protocol and a path', () => {
-  expect(extractDomain(`https://${EXAMPLE_COM}/about`))
-    .toEqual(EXAMPLE_COM);
+  expectExampleCom(`https://${EXAMPLE_COM}/about`);
 });
 
 test('url has a protocol and path with multiple slashes', () => {
-  expect(extractDomain(`https://${EXAMPLE_COM}///about`))
-    .toEqual(EXAMPLE_COM);
+  expectExampleCom(`https://${EXAMPLE_COM}///about`);
 });
